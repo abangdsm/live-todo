044e9bfd30342b10832634d034e630735005808f
abangdsm <id.reversal@gmail.com>
2025-05-23 10:02:20 +0700

    Menambahkan database dan fungsi simpan pada create box

--- app/Livewire/TodoList.php
+++ app/Livewire/TodoList.php
@@ -2,14 +2,22 @@
 
 namespace App\Livewire;
 
+use App\Models\Todo;
 use Livewire\Component;
+use Livewire\Attributes\Rule;
 
 class TodoList extends Component
 {
+    #[Rule('required|min:3|max:50')]
     public $name;
 
+    public $search;
+
     public function create(){
-        // 
+        $validated = $this->validateOnly('name');
+        Todo::create($validated);
+        $this->reset('name');
+        session()->flash('success', 'Todo has been Created');
     }
 
     public function render()
